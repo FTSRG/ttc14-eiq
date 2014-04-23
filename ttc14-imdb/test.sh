e668193f97ecc418fcbd18a93a75f4ce287b06e6
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-23 12:09:47 +0200

    test sh script, cleanup

--- ttc14-imdb/test.sh
+++ ttc14-imdb/test.sh
@@ -7,7 +7,7 @@ vmargs="-XX:MaxPermSize=256m -Xms5G -Xmx5G"
 for n in $sizes; do
 
 	echo $n
-	java $vmargs -jar hu.bme.mit.ttc.imdb.generator/target/hu.bme.mit.ttc.imdb.generator-1.0.0-SNAPSHOT.jar -instanceModel /home/szarnyasg/git/ttc14-eiq/ttc14-imdb/hu.bme.mit.ttc.imdb.instance/model/synthetic-$n.movies -N $n
-	#java $vmargs -jar hu.bme.mit.ttc.imdb.transformation/target/hu.bme.mit.ttc.imdb.transformation-1.0.0-SNAPSHOT.jar -instanceModel /home/szarnyasg/git/ttc14-eiq/ttc14-imdb/hu.bme.mit.ttc.imdb.instance/model/synthetic-$n.movies -N $n
+	java $vmargs -jar hu.bme.mit.ttc.imdb.generator/target/hu.bme.mit.ttc.imdb.generator-1.0.0-SNAPSHOT.jar -instanceModelDir /home/szarnyasg/git/ttc14-eiq/ttc14-imdb/hu.bme.mit.ttc.imdb.instance/model -N $n
+	java $vmargs -jar hu.bme.mit.ttc.imdb.transformation/target/hu.bme.mit.ttc.imdb.transformation-1.0.0-SNAPSHOT.jar -instanceModelPath /home/szarnyasg/git/ttc14-eiq/ttc14-imdb/hu.bme.mit.ttc.imdb.instance/model/synthetic-$n.movies
 
 done
