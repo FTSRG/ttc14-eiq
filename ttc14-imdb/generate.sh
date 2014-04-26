3cc6033634c758078bf7dfd41919a152ecc29ba0
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-26 16:36:33 +0200

    added compatiblity for CYGWIN

--- ttc14-imdb/generate.sh
+++ ttc14-imdb/generate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-java -cp hu.bme.mit.ttc.imdb.generator/target/hu.bme.mit.ttc.imdb.generator-1.0.0-SNAPSHOT.jar:hu.bme.mit.ttc.imdb.generator/target/lib/* hu.bme.mit.ttc.imdb.main.GeneratorMain -instanceModelDir /home/szarnyasg/git/ttc14-eiq/ttc14-imdb/hu.bme.mit.ttc.imdb.instance/model -N 1
+if [ "$(expr substr $(uname -s) 1 6)" = "CYGWIN" ]; then
+	separator=";"
+else
+	separator=":"
+fi
+
+java -cp "hu.bme.mit.ttc.imdb.generator/target/hu.bme.mit.ttc.imdb.generator-1.0.0-SNAPSHOT.jar"$separator"hu.bme.mit.ttc.imdb.generator/target/lib/*" hu.bme.mit.ttc.imdb.main.GeneratorMain -instanceModelDir hu.bme.mit.ttc.imdb.instance/model -N 1
