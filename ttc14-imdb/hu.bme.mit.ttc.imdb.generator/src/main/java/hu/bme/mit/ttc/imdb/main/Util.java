39e14fa4206d7b2de1679c28c646f978a5638c42
Semerath <Semerath@Semerath-Laptop>
2014-04-29 17:31:32 +0200

    This resource implementation is definitely better to match pattern on it.

--- ttc14-imdb/hu.bme.mit.ttc.imdb.generator/src/main/java/hu/bme/mit/ttc/imdb/main/Util.java
+++ ttc14-imdb/hu.bme.mit.ttc.imdb.generator/src/main/java/hu/bme/mit/ttc/imdb/main/Util.java
@@ -3,26 +3,30 @@ package hu.bme.mit.ttc.imdb.main;
 import hu.bme.mit.ttc.imdb.movies.MoviesPackage;
 //import hu.bme.mit.ttc.imdb.util.XMIResourceFactoryImplWithUUID;
 
+
+import hu.bme.mit.ttc.imdb.util.XMIResourceFactoryImplWithUUID;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 //import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 public class Util {
 	
 	public static void registerStandaloneEMFPackages() {
 		
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
-//		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("movies", new XMIResourceFactoryImpl());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("movies", new XMIResourceFactoryImpl());
 //		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("movies", new XMIResourceFactoryImplWithUUID());
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("movies", new Resource.Factory() {
-
-			public Resource createResource(URI arg0) {
-					return new BinaryResourceImpl(arg0);				
-			}
-			
-		});
+//		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("movies", new Resource.Factory() {
+//
+//			public Resource createResource(URI arg0) {
+//					return new BinaryResourceImpl(arg0);				
+//			}
+//			
+//		});
 		MoviesPackage.eINSTANCE.eClass();
 	}
 
